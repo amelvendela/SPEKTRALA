aa18c960254147e32955684f342f5e1c5a3e04a6
Kimberly Nguyen <ktqng@kth.se>
2019-12-02 10:56:16 +0100

    block

--- Lab1.py
+++ Lab1.py
@@ -3,14 +3,30 @@ import numpy as np
 import scipy
 import scipy.signal
 import imageio
+import math
+
+
+def block(i):
+    bs = 8
+    c = np.zeros(i.shape)
+    for p in range(0, math.floor(i.shape[0] / bs)):
+        for q in range(0, math.floor(i.shape[1] / bs)):
+            isub = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]  # Man skapar 8x8 block
+            average = np.mean(isub)
+
+            # Skapa prick
+
+            c[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = average  # Sätter ihop alla block
+    return c
 
 
 stockholm = imageio.imread("sthlm.jpg")
-
-
-plt.imshow(stockholm, cmap='gray')
-plt.title('Stockholm')
+bw_stockholm = np.mean(stockholm, axis=2)
+plt.imshow(bw_stockholm, cmap="gray")
+plt.title("Stockholm original")
 plt.show()
 
-
-"hej"
+mean_image = block(stockholm)
+plt.imshow(mean_image.astype('uint8'), cmap="gray")
+plt.title("Stockholm block")
+plt.show()
