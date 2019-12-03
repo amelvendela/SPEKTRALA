e651367281375bf7ccf740ff3c6f304dac36d91d
Kimberly Nguyen <ktqng@kth.se>
2019-12-03 10:03:18 +0100

    Variabeländringar

--- Lab1.py
+++ Lab1.py
@@ -4,30 +4,30 @@ import imageio
 import math
 
 
-def block(i):
+def grey_averages(i):
     bs = 8
-    c = np.zeros(i.shape)
+    all_blocks = np.zeros(i.shape)
     for p in range(0, math.floor(i.shape[0] / bs)):
         for q in range(0, math.floor(i.shape[1] / bs)):
-            isub = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]  # Man skapar 8x8 block
-            average = np.average(isub)
-            pixel_amount = int(average * (isub.size / 255))
-            circle = 
+            block = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]  # Man skapar 8x8 block
+            average = np.average(block)
+            pixel_amount = int(average * (block.size / 255))
             m = np.zeros(bs*bs)
             for h in range(pixel_amount):
                 m[h] = 255
             m = m.reshape((bs, bs))
-            c[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = m  # Sätter ihop alla block
+            all_blocks[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = m  # Sätter ihop alla block
 
-    return c
+    return all_blocks
 
 
-uggla = imageio.imread("uggla2.tif")
-plt.imshow(uggla, cmap="gray")
-plt.title("Ugglis original")
+sthlm = imageio.imread("sthlm.jpg")
+sthlm_bw = np.mean(sthlm, axis=2)
+plt.imshow(sthlm, cmap="gray")
+plt.title("Stockholm original")
 plt.show()
 
-mean_image = block(uggla)
+mean_image = grey_averages(sthlm_bw)
 plt.imshow(mean_image.astype('uint8'), cmap="gray")
-plt.title("Ugglis block")
+plt.title("Stockholm block")
 plt.show()
