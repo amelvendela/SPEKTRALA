c073dffdae079fc0756be2613b35d2b04384a771
Kimberly Nguyen <ktqng@kth.se>
2019-12-03 09:54:26 +0100

    Error diffusion

--- Lab1.py
+++ Lab1.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
-import scipy.signal
 import imageio
 import math
 
@@ -12,21 +10,24 @@ def block(i):
     for p in range(0, math.floor(i.shape[0] / bs)):
         for q in range(0, math.floor(i.shape[1] / bs)):
             isub = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]  # Man skapar 8x8 block
-            average = np.mean(isub)
+            average = np.average(isub)
+            pixel_amount = int(average * (isub.size / 255))
+            circle = 
+            m = np.zeros(bs*bs)
+            for h in range(pixel_amount):
+                m[h] = 255
+            m = m.reshape((bs, bs))
+            c[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = m  # Sätter ihop alla block
 
-            # Skapa prick
-
-            c[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = average  # Sätter ihop alla block
     return c
 
 
-stockholm = imageio.imread("sthlm.jpg")
-bw_stockholm = np.mean(stockholm, axis=2)
-plt.imshow(bw_stockholm, cmap="gray")
-plt.title("Stockholm original")
+uggla = imageio.imread("uggla2.tif")
+plt.imshow(uggla, cmap="gray")
+plt.title("Ugglis original")
 plt.show()
 
-mean_image = block(stockholm)
+mean_image = block(uggla)
 plt.imshow(mean_image.astype('uint8'), cmap="gray")
-plt.title("Stockholm block")
+plt.title("Ugglis block")
 plt.show()
