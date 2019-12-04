6e96e917a47f6312bfafc09ea88acee1e6360a37
Kimberly Nguyen <ktqng@kth.se>
2019-12-04 13:18:36 +0100

    Floydfil

--- Floyd.py
+++ Floyd.py
@@ -4,33 +4,39 @@ import imageio
 import math
 
 
-def grey_averages(i):
-    bs = 8
+def halftoning(i, bs):
+    i = 255 - i
     all_blocks = np.zeros(i.shape)
+
     for p in range(0, math.floor(i.shape[0] / bs)):
         for q in range(0, math.floor(i.shape[1] / bs)):
-            block = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]  # Man skapar 8x8 block
-            greyscale_average = np.average(block)
-            pixel_amount = int(greyscale_average * (block.size / 255))
+            block = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]  # Skapar blocken
 
-            m = np.zeros(bs*bs)
-            for h in range(pixel_amount):
-                m[h] = 255
-            m = m.reshape((bs, bs))
+            greytone_average = np.average(block)
+            pixel_amount = int(greytone_average * (block.size / 255))  # får inte vara fler pixlar än 64 per ruta
+
+            radius = np.sqrt(pixel_amount / np.pi)
+            m = np.zeros((bs, bs))  # Skapar en matris
+
+            for x in range(bs):  # Går igenom varje pixel och sätter värden till vår matris
+                for y in range(bs):
+                    if radius <= np.sqrt((x-(bs / 2)) ** 2 + (y - (bs / 2))**2):  # cirkelns ekvation
+                        m[x, y] = 255
+                    else:
+                        m[x, y] = 0
+
             all_blocks[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = m  # Sätter ihop alla block
+
     return all_blocks
 
 
-size_circle = 4
-
-
-sthlm = imageio.imread("sthlm.jpg")
-sthlm_bw = np.mean(sthlm, axis=2)
-plt.imshow(sthlm_bw, cmap="gray")
-plt.title("Stockholm original")
+image = imageio.imread("sthlm.jpg")
+image_bw = np.mean(image, axis=2)
+plt.imshow(image_bw, cmap="gray")
+plt.title("Original image")
 plt.show()
 
-mean_image = grey_averages(sthlm_bw)
+mean_image = halftoning(image_bw, 4)
 plt.imshow(mean_image.astype('uint8'), cmap="gray")
-plt.title("Stockholm block")
+plt.title("Halftoned image")
 plt.show()
