29dd7be14bb7d98b5f48d493d54c5e08be402f44
Kimberly Nguyen <ktqng@kth.se>
2019-12-05 15:00:48 +0100

    halvtoning funktion tillbaka

--- Lab1.py
+++ Lab1.py
@@ -6,49 +6,37 @@ import math
 
 def halftoning(i, bs):
     i = 255 - i
-    floyd = np.zeros(i.shape)
-    size = i.size
-
-    error = np.zeros(size(i))
-
+    bs = 8
     all_blocks = np.zeros(i.shape)
+
+    # dela upp bilden i block
 
     for p in range(0, math.floor(i.shape[0] / bs)):
         for q in range(0, math.floor(i.shape[1] / bs)):
             block = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]  # Skapar blocken
 
+            # mäter blockets gråtonsmedelvärde
+
             greytone_average = np.average(block)
             pixel_amount = int(greytone_average * (block.size / 255))  # får inte vara fler pixlar än 64 per ruta
 
-            quant_error = i - floyd
+            radius = np.sqrt(pixel_amount / np.pi)
+            m = np.zeros((bs, bs))  # Skapar en matris
 
-            for x in range(0, bs):
-                for y in range(0, bs):
-                    if x < bs:
-                        error[x+1, y] = error[x, y+1] + (quant_error*5/16)
+            # går igenom alla pixlar
+            for x in range(bs):  # Går igenom varje pixel och sätter värden till vår matris
+                for y in range(bs):
+                    if radius <= np.sqrt((x-(bs / 2)) ** 2 + (y - (bs / 2))**2):  # cirkelns ekvation
+                        m[x, y] = 255
+                    else:
+                        m[x, y] = 0
 
-                    elif y < bs:
-                        error[x, y+1] = error[x, y+1] + quant_error*3/16
-
-                    elif x>1:
-                        error[x-1, y+1] = error[x-1, y+1] + quant_error*3/16
-                    elif x < bs:
-                        error[x+1, y+1] = error[x+1, y+1] + quant_error*1/16
-
-
-
-
-
-
+            all_blocks[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = m  # Sätter ihop alla block
 
     return all_blocks
 
-sthlm = imageio.imread("katter.jpg")
-sthlm_bw = np.mean(sthlm, axis=2)
-plt.imshow(sthlm_bw, cmap="gray")
-plt.title("Stockholm original")
 
-image = imageio.imread("sthlm.jpg")
+image = imageio.imread("katter.jpg")
 image_bw = np.mean(image, axis=2)
 plt.imshow(image_bw, cmap="gray")
 plt.title("Original image")
