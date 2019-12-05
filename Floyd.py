ce604bd2855df0e59e1240a249be28bd748dd919
Kimberly Nguyen <ktqng@kth.se>
2019-12-05 14:53:14 +0100

    fungerande floyd steinberg funktion

--- Floyd.py
+++ Floyd.py
@@ -1,64 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import imageio
-import math
-
-
-def greys(i, bs):
-    i = 255 - i
-    full_i = np.zeros(i.shape)
-    for p in range(0, math.floor(i.shape[0] / bs)):
-        for q in range(0, math.floor(i.shape[1] / bs)):
-            block = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]  # Skapar blocken
-
-            greytone_average = np.average(block)
-            pixel_amount = int(greytone_average * (block.size / 255))  # får inte vara fler pixlar än 64 per ruta
-
-            m = np.zeros(bs*bs)
-            for h in range(pixel_amount):  # Går igenom varje pixel och sätter värden till vår matris
-                m[h] = 255
-
-            m = m.reshape((bs, bs))
-            full_i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = m  # Sätter ihop alla block
-
-    return full_i
 
 
 def floyd_steinberg(i):
     floyd = np.zeros(i.shape)
     height, width = i.shape
-    pixel = greys(i, 8)
-    factor = 1
-    for x in range(1, width-1):
-        for y in range(0, height-1):
-            old_pixel = pixel[x][y]
-            new_pixel = np.round(factor * old_pixel / 255) * (255 / factor)
-            pixel[x][y] = new_pixel
-            quant_error = old_pixel - new_pixel
 
-            floyd[x + 1][y] = floyd[x+1][y] + quant_error * 7 / 16
-            floyd[x-1][y+1] = floyd[x-1][y+1] + quant_error * 3 / 16
-            floyd[x][y+1] = floyd[x][y+1] + quant_error * 5 / 16
-            floyd[x+1][y+1] = floyd[x+1][y+1] + quant_error * 1 / 16
+    for x in range(1, height-1):
+        for y in range(0, width-1):
 
+            if i[x, y] < 127.5:
+                floyd[x, y] = 0
+            else:
+                floyd[x, y] = 255
+
+            quant_error = i[x, y] - floyd[x, y]
+            i[x + 1][y] = i[x+1][y] + quant_error * 7 / 16
+            i[x][y + 1] = i[x][y + 1] + quant_error * 5 / 16
+            i[x-1][y+1] = i[x-1][y+1] + quant_error * 3 / 16
+            i[x+1][y+1] = i[x+1][y+1] + quant_error * 1 / 16
 
     return floyd
-
-
-'''quant_error = i - all_blocks
-
-            for p in range(0, math.floor(i.shape[0] / bs)):
-                for q in range(0, math.floor(i.shape[1] / bs)):
-                    # Extract an 8x8 sub matrix 'Isub' from the original image
-                    Isub = i[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs]
-
-                    # Put matrix operation under this line
-                    new_isub = Isub / all_blocks
-                    new_isub = np.trunc(new_isub)
-                    P[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] = new_isub
-                    P[p * bs: (p + 1) * bs, q * bs: (q + 1) * bs] *= w_matrix
-
-    return all_blocks'''
 
 
 image = imageio.imread("sthlm.jpg")
@@ -69,5 +32,5 @@ plt.show()
 
 mean_image = floyd_steinberg(image_bw)
 plt.imshow(mean_image.astype('uint8'), cmap="gray")
-plt.title("Halftoned image")
+plt.title("Floyd-Steinberg image")
 plt.show()
